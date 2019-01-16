b09d7e627fed1dc4107871f284c6a1a58adc956a
EunjinKim <eunjinn.kim@linecorp.com>
2019-01-16 17:00:00 +0900

    add class

--- greeter.js
+++ greeter.js
@@ -1,6 +1,19 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        // public은 해당 이름으로 속성을 자동으로 생성할 수 있는 단축형태
+        /*
+        public firstName -> this.firstName = firstName
+        * */
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
     return "hello " + person.firstName + " " + person.lastName;
 }
-var user = { firstName: 'Jane', lastName: 'User' };
-// const user = [0, 1, 2];
+// const user = { firstName: 'Jane', lastName: 'User'};
+var user = new Student('Jane', 'M.', 'User');
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,14 @@
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        // public은 해당 이름으로 속성을 자동으로 생성할 수 있는 단축형태
+        /*
+        public firstName -> this.firstName = firstName
+        * */
+        this.fullName = `${firstName} ${middleInitial} ${lastName}`;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -6,6 +17,7 @@ interface Person {
 function greeter(person: Person) {
     return `hello ${person.firstName} ${person.lastName}`;
 }
-const user = { firstName: 'Jane', lastName: 'User'};
-// const user = [0, 1, 2];
+
+// const user = { firstName: 'Jane', lastName: 'User'};
+const user = new Student('Jane', 'M.', 'User');
 document.body.innerHTML = greeter(user);
